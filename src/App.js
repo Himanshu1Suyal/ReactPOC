e47b898fba2c8e6285d3ea46fe2ca8501406178f
Himanshu Suyal <HimanshuSu@hcl.com>
2019-10-16 13:43:40 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -8,11 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-         Hello World
-      
+          Hello World
+         </p>
       </header>
     </div>
   );

3b0b6187c3f6e020e8bcd752a325573da3508383
Himanshu Suyal <HimanshuSu@hcl.com>
2019-10-17 13:03:10 +0530

    Uses of Props and State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-     var i=1;
-      return (
-         <div>
-           <h1>
-             {i===1 ? 'true' :'False'}
-           </h1>
-         </div>
+constructor(props){
+  super(props);
+  this.state = {
 
-      );
+    header:"This is header",
+    content:"This is content"
+
    }
+  }
+
+  render(){
+    return (
+      <div>
+     <Header headerProp = {this.state.header}/> 
+     <Content contentProp = {this.state.content}/> 
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>{this.props.headerProp}</h1>
+
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>{this.props.contentProp}</h1>
+
+      </div>
+    );
+  }
 }
 export default App;

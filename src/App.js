9247dda9539556687e65219b46611b628995e63f
Himanshu Suyal <HimanshuSu@hcl.com>
2019-10-17 17:08:05 +0530

    use of PropTypes  : its validation and its implementation

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class App extends React.Component {
-constructor(props){
-  super(props);
-  this.state = {
-
-    header:"This is header",
-    content:"This is content"
-
+   render() {
+      return (
+         <div>
+            <h3>Array: {this.props.propArray}</h3>
+            <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
+            <h3>Func: {this.props.propFunc(3)}</h3>
+            <h3>Number: {this.props.propNumber}</h3>
+            <h3>String: {this.props.propString}</h3>
+            <h3>Object: {this.props.propObject.objectName1}</h3>
+            <h3>Object: {this.props.propObject.objectName2}</h3>
+            <h3>Object: {this.props.propObject.objectName3}</h3>
+         </div>
+      );
    }
-  }
-
-  render(){
-    return (
-      <div>
-     <Header headerProp = {this.state.header}/> 
-     <Content contentProp = {this.state.content}/> 
-      </div>
-    );
-  }
 }
 
-class Header extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>{this.props.headerProp}</h1>
-
-      </div>
-    );
-  }
+App.propTypes = {
+   propArray: PropTypes.array.isRequired,
+   propBool: PropTypes.bool.isRequired,
+   propFunc: PropTypes.func,
+   propNumber: PropTypes.number,
+   propString: PropTypes.string,
+   propObject: PropTypes.object
 }
 
-class Content extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>{this.props.contentProp}</h1>
-
-      </div>
-    );
-  }
+App.defaultProps = {
+  // propArray: [1,2,3,4,5],
+   propBool: true,
+   propFunc: function(e){return e},
+   propNumber: 1,
+   propString: "String value...",
+   
+   propObject: {
+      objectName1:"Object first",
+      objectName2: "object second",
+      objectName3: "object Third"
+   }
 }
 export default App;

e15d33dd71cdcd9108391e7673560d4d6d572792
Himanshu Suyal <HimanshuSu@hcl.com>
2019-10-18 17:48:48 +0530

    setState React Component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+//Use of setState Component
+
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <h3>Array: {this.props.propArray}</h3>
-            <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
-            <h3>Func: {this.props.propFunc(3)}</h3>
-            <h3>Number: {this.props.propNumber}</h3>
-            <h3>String: {this.props.propString}</h3>
-            <h3>Object: {this.props.propObject.objectName1}</h3>
-            <h3>Object: {this.props.propObject.objectName2}</h3>
-            <h3>Object: {this.props.propObject.objectName3}</h3>
-         </div>
-      );
-   }
-}
+   constructor(){
+    super();
 
-App.propTypes = {
-   propArray: PropTypes.array.isRequired,
-   propBool: PropTypes.bool.isRequired,
-   propFunc: PropTypes.func,
-   propNumber: PropTypes.number,
-   propString: PropTypes.string,
-   propObject: PropTypes.object
-}
+            this.state={
+              data : []
+            }
+            this.setStateHandler = this.setStateHandler.bind(this);
+   };
+   setStateHandler() {
+                        var item="Set the data  , ";
+                        var myArray = this.state.data.slice();
+                        myArray.push(item);
+                        this.setState({ data: myArray})
+                    };
 
-App.defaultProps = {
-  // propArray: [1,2,3,4,5],
-   propBool: true,
-   propFunc: function(e){return e},
-   propNumber: 1,
-   propString: "String value...",
-   
-   propObject: {
-      objectName1:"Object first",
-      objectName2: "object second",
-      objectName3: "object Third"
-   }
+                    render() {
+                      return (
+                        <div>
+                              <button onClick= { this.setStateHandler}>SET STATE</button>
+                              <h2>  State Array : {this.state.data}</h2>
+
+                        </div>
+                      );
+                    }
+
 }
 export default App;
